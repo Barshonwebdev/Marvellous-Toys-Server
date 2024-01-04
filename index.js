720ecba82bbde4c68cf8710a58e64da9b54afb2f
Barshon <barshonweb@gmail.com>
2024-01-05 00:27:58 +0600

    server link fixed!

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const app=express();
 const cors = require('cors');
+const app=express();
+app.use(cors());
 require('dotenv').config();
 const port=process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 //middleware
-app.use(cors());
 app.use(express.json());
 
 //api
