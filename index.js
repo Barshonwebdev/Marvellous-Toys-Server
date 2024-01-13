8192d1e077b26ae81c5d279f559da088c4dd47c2
Barshon <barshonweb@gmail.com>
2024-01-13 11:25:04 +0600

    server fixing

--- index.js
+++ index.js
@@ -36,8 +36,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   
 
     //all toys api
     const alltoysCollection=client.db('toydb').collection('alltoys');
